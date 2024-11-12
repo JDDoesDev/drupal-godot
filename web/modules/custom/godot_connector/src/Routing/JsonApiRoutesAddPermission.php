46d8bbe086c8b6bb7d7e6616c750f73659982192
JD Flynn <jflynn8@gmail.com>
2024-11-11 20:29:37 -0600

    reverted oauth2_server shit

---
+++ web/modules/custom/godot_connector/src/Routing/JsonApiRoutesAddPermission.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\godot_connector\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class JsonApiRoutesAddPermission.
+ *
+ * Adds a specific permission to JSON:API routes.
+ */
+// class JsonApiRoutesAddPermission extends RouteSubscriberBase {
+
+// //   /**
+// //    * {@inheritdoc}
+// //    */
+// //   protected function alterRoutes(RouteCollection $collection) {
+// //     foreach ($collection as $route) {
+// //       $defaults = $route->getDefaults();
+// //       if (!empty($defaults['_is_jsonapi'])) {
+// //         $route->setRequirement('_permission', 'access jsonapi routes');
+// //       }
+// //     }
+// //   }
+// }
